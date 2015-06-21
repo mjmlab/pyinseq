f718bf2d4391b535da9e063edd92c5635d929b71
Mark Mandel <mandel01@gmail.com>
2015-06-21 09:24:41 -0500

    include barcodes and bowtie path in config file

--- config.py
+++ config.py
@@ -1,33 +1,23 @@
-""" Configuration file for pyinseq.py package
+""" Configuration file for pyinseq.py package """
 
-Edit each of the variables below as appropriate for current sample.
-XXXXX IS THERE A MORE USER-FRIENDLY WAY TO POPULATION THESE FIELDS? XXXXX """
-
-
-
-""" Barcodes for this sample
-
-5' 4-nt barcodes """
-
+# Barcodes (5' barcodes)
 barcodes = { \
-'GTAC':'Input1','AAAA':'Input2', \
-'TATA':'Input3','GTCA':'Input4', \
-'TTAA':'Output5','AACC':'Output6', \
-'GCTA':'Output7','AGTC':'Output8'}
+'CGAT':'Input1', \
+'GCTA':'Input2', \
+'AGTC':'Output1', \
+'AAAA':'Output2'}
 
-
-""" Mariner transposon end sequences
-
-In many cases the transposon sequence will be the same for both ends
-XXXXX Insert comment about TA site """
-
+# Mariner transposon end sequences
+# In many cases the transposon sequence will be the same for both ends
+# XXXXX Insert comment about TA site
 transposon_left = ''
 transposon_right = ''
 
-
-
-""" Genome files """
-
+# Genome files. Files in same order for all sections below (e.g., Ch1, plasmid)
 fasta = ''
 ptt = ''
-rpt = ''
+rnt = ''
+
+# Path to Bowtie
+bowtie = '/Users/markmandel/bowtie-1.1.1/bowtie'
+bowtie-build = '/Users/markmandel/bowtie-1.1.1/bowtie-build'
