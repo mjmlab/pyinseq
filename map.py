e0b6a7b976ba5343b9850189971ba5187b81dc5a
Mark Mandel <mandel01@gmail.com>
2015-06-29 21:42:07 -0500

    bowtie-build seems to work

--- map.py
+++ map.py
@@ -12,7 +12,23 @@ import subprocess
 import config # config file
 
 def bowtie_build():
-    subprocess.check_call([config.bowtie_build])
+    organism = 'k'
+    fna = organism + '.fna'
+    print('\n===== Building bowtie index files for organism {} =====\n'.format(organism))
+    subprocess.check_call([config.bowtie_build, '-q', fna, organism])
+
+def bowtie_map():
+
+
+    """
+
+    from Goodman:
+
+    # mismatch replaced with 0
+
+    $bowtie_path/bowtie -m 1 --best --strata -a --fullref -n 0 -l 17  $path\/indexes\/$index\/$index -f bcsortedseqs\/$input\_$codes_hash{$_}\.fas results\/$input\_$codes_hash{$_}\.bowtiemap\n
+
+    """
 
 
 # ===== Start here ===== #
