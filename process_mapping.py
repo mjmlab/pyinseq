2605ce705797520e94e3cb169334706b2765b237
Mark Mandel <mandel01@gmail.com>
2015-07-16 01:34:20 -0500

    multifasta2dict works to convert fasta to dictionary

--- process_mapping.py
+++ process_mapping.py
@@ -10,6 +10,87 @@ import sys
 import re
 from collections import Counter
 
+def multifasta2dict(fna):
+    """
+    Returns a dictionary of fasta header and sequences
+
+    Input file in multifasta format
+    >header1
+    agctag
+    >header2
+    gattta
+
+    Function returns the fasta data as a dictionary
+    {'header1': 'agctag', 'header2': 'gattta'}
+
+    """
+    sequence_dict = {}
+    with open(fna, 'r') as fi:
+        header = ''
+        first_line = False # next line is first line of sequence in contig
+        for line in fi:
+            line = line.rstrip()
+            if line.startswith('>'): # header line; therefore new contig
+                header = line[1:] # new header
+                first_line = True
+            else: # sequence line
+                if first_line:
+                    sequence_dict[header] = line # create dictionary entry
+                    first_line = False
+                # would this work in one step?
+                else:
+                    appended_sequence = sequence_dict[header] + line
+                    sequence_dict[header] = appended_sequence
+        print(sequence_dict)
+        return sequence_dict
+
+def TA_sites(fna):
+    """ List TA dinucleotides in a fasta nucleotide file
+
+    Returns a list of nucleotide positions for
+    5'-TA for each contig in the fna file
+    e.g., CP000020 = (20, 22, 24, ... 1401104)
+    #INSERT THE REAL DATA HERE! HOW DOES IT WORK FOR MULTIFASTA?
+    Assumes circular genome
+    i.e. will check if the last nucleotide is a T and the first is an A
+    """
+    with open(fna, 'r') as fi:
+        ta_list = []
+        i = 0   # nucleotide index
+        # Checks the linear molecule (i.e. all except the last base)
+        while i < len(fna_string)-1:
+            if fna_string[i:i+2] == 'ta':
+                ta_list.append(i+1)
+            i += 1
+        # Checks last base circular around to first base
+        if i == len(fna_string)-1:
+            if (fna_string[-1] + fna_string[0]) == 'ta':
+                ta_list.append(i+1)
+        print(ta_list)
+
+        # NEED TO RENAME IT AS THE CONTIG NAME.
+
+
+        contig_length = len(fna_string)
+
+        print(contig_name)
+        print(len(fna_string))
+
+        """for n in line.rstrip():
+            print(len(n))
+            fna_list.append(n)
+            if(len(fna_list) == 2):
+                # special case of last base matching
+                # need robust way to count number of bases
+                # and print its position here
+                pass
+            else:
+                if fna_list[-2:] == ['t','a']:
+                    print(len(fna_list)-2)
+            if len(fna_list) > 100:
+                exit(0)"""
+
+
 def count_bowtie(bowtie_output):
     """ List counts of reads to the Left and Right of each TA site
 
@@ -35,80 +116,11 @@ def count_bowtie(bowtie_output):
             for c in count:
                 print('{0}\t{1}'.format(c, count[c]))
 
-def TA_sites(fna):
-    """ List TA dinucleotides in a fasta nucleotide file
-
-    Returns a list of TA sites for each contig in the fna file
-    e.g., CP000020 = (20, 22, 24, ... 1401104)
-    #INSERT THE REAL DATA HERE! HOW DOES IT WORK FOR MULTIFASTA?
-    Assumes circular genome
-    i.e. will check if the last nucleotide is a T and the first is an A
-    """
-    with open(fna, 'r') as fi:
-        outfile = fna + '_ta_sites.txt'
-        with open(outfile, 'w') as fo:
-            count = 0
-            # need to deal with last base in contig
-            fna_string = ''
-            contig_name = ''
-            for line in fi:
-                if line.startswith('>'):
-                    if len(fna_string) > 0:
-                        # do something with the old DNA sequence before
-                        # starting with the new contig
-                        pass
-                    contig_name = line.rstrip('\n')[1:]
-                    fna_string = ''  # New contig...clear the DNA sequence
-                else:
-                    fna_string = fna_string + line.strip('\n')
-
-            # START WORKING HERE
-            ta_list = []
-            i = 0   # nucleotide index
-            # Checks the linear molecule (i.e. all except the last base)
-            while i < len(fna_string)-1:
-                if fna_string[i:i+2] == 'ta':
-                    ta_list.append(i+1)
-                i += 1
-            # Checks last base circular around to first base
-            if i == len(fna_string)-1:
-                if (fna_string[-1] + fna_string[0]) == 'ta':
-                    ta_list.append(i+1)
-            print(ta_list)
-
-            # NEED TO RENAME IT AS THE CONTIG NAME.
-            # STILL NEED TO FIGURE OUT MULTIFASTA
-
-
-            contig_length = len(fna_string)
-
-            print(contig_name)
-            print(len(fna_string))
-
-            # How best to return the list of lists (for a multifasta)?
-
-
-            """for n in line.rstrip():
-                print(len(n))
-                fna_list.append(n)
-                if(len(fna_list) == 2):
-                    # special case of last base matching
-                    # need robust way to count number of bases
-                    # and print its position here
-                    pass
-                else:
-                    if fna_list[-2:] == ['t','a']:
-                        print(len(fna_list)-2)
-                if len(fna_list) > 100:
-                    exit(0)"""
-
-
-
-def map_to_gene(bowtie_output):
+def normalize_cpm(bowtie_output):
     """ insert """
     pass
 
-def map_to_gene(bowtie_output):
+def map_to_gene(normalized_output):
     """ insert """
     pass
 
@@ -117,7 +129,7 @@ def map_to_gene(bowtie_output):
 
 def main():
     fna = sys.argv[1]
-    TA_sites(fna)
+    multifasta2dict(fna)
 
 if __name__ == "__main__":
     main()
