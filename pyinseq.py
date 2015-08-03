30a7e2266620fe82151d47be4f8edb863ca4a348
Mark Mandel <mandel01@gmail.com>
2015-08-02 23:51:36 -0500

    add in steps for assigning barcodes and transposon

--- pyinseq.py
+++ pyinseq.py
@@ -6,6 +6,7 @@ Main script for running the pyinseq package
 
 from assign import *
 from gbkconvert import *
+from map_reads import *
 import sys
 import os
 import argparse
@@ -23,6 +24,15 @@ def parse_args(args):
         required=True)
     return parser.parse_args(args)
 
+# Change to the genome directory and run bowtie_build() and bowtie_map()
+class cd_bowtie:
+    def __init__(self, temp_dir, organism, reads, bowtie_output):
+        self.savedPath = os.getcwd()
+        os.chdir(temp_dir)
+        bowtie_build(organism)
+        bowtie_map(organism, reads, bowtie_output)
+    def __del__(self):
+        os.chdir(self.savedPath)
 
 # ===== Start here ===== #
 
@@ -31,19 +41,31 @@ def main():
     #print(args.input)
     #print(args.samples)
     #assign_and_trim(args.input, args.samples)
-    inputfile = sys.argv[1]
+    gbkfile = sys.argv[1]
     organism = sys.argv[2]
     experiment = sys.argv[3]
+    reads = sys.argv[4]
+    samples = sys.argv[5]
+
+    #pyinseq_directory = os.getcwd()
+    temp_dir = '{0}/temp/'.format(experiment)
+
 
     # Create the directory struture based on the experiment name
     create_directories(experiment)
 
     # Prepare genome files from the GenBank input
-    gbk2fna(inputfile, organism, experiment)
-    gbk2ftt(inputfile, organism, experiment)
+    gbk2fna(gbkfile, organism, experiment, temp_dir)
+    gbk2ftt(gbkfile, organism, experiment, temp_dir)
+
+    # Assign and trim barcodes
+    assign_and_trim(reads, samples, experiment, temp_dir)
+
+
 
     # Prepare the bowtie indexes
-    
+    # Map the reads using bowtie_map
+    #x = cd_bowtie(temp_dir, organism, reads, '../bowtie_output.txt')
 
 
 if __name__ == '__main__':
