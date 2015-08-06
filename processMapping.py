320f29f9cb00551946bdfc46bd4762abd5a6d3ae
Mark Mandel <mandel01@gmail.com>
2015-08-06 11:06:37 -0500

    works to add L and R counts. Could use a lot of optimization love

--- processMapping.py
+++ processMapping.py
@@ -9,7 +9,8 @@ Future - filter on the 16/17 bp positions before this step; maybe even before bo
 
 import sys
 import re
-from collections import Counter
+import csv
+import collections
 from operator import itemgetter
 
 def multifasta2dict(fna):
@@ -72,25 +73,33 @@ def taSites(fna):
                 do[header].append(i+1)
     return do
 
-def insertionNucleotides(bowtieOutput):
+def insertionNucleotides(bowtieOutput, experiment=''):
     """ Define the TA dinucleotide of the insertion from the bowtie output
 
     Note bowtie output, where bnt = bowtie nucleotide
     + strand (sequence to L of Tn):
-    bnt_sequence_TA
+    <bnt><chromosomeSequence><TA> (TA is part of the sequence)
     - strand (sequence to R of Tn):
-    bnt_TA_sequence
+    <bnt><TA><chromosomeSequence> (TA is part of the sequence)
 
     Returns a list of tuples for the insertion nucleotides and orientations.
     Each insertion read is listed separately:
     experiment, barcode, contig, TAnucleotide, orientation
     [('Exp002', 'AAAA', 'contig1', 514141, 'R'), ('Exp003', 'GCTA', 'contig5', 8141, 'L')]
 
-    CHECK -- DID MY TEST FNA FILE SHIFT BY 1 NT?
+    Print output tab-delimited in a file called <experiment>_bowtieOutput_InsertionDetail.txt
 
     """
     with open(bowtieOutput, 'r') as fi:
-        insertions = []
+        # Create new output document or overwrite previous document
+
+        ## Pass the temp directory here in the future?
+        ## Make this consistent in all of the scripts
+        ## Also can only pass experiment name and recreate the rest.
+
+        with open('{0}/temp/{0}_bowtieOutput_InsertionDetail.txt'.format(experiment), 'w') as fo:
+            pass
+        #insertions = []
         for line in fi:
             # Redo this with regex
             idLine = line.rstrip().split('\t')[0]
@@ -109,17 +118,21 @@ def insertionNucleotides(bowtieOutput):
             readLength = len(line.rstrip().split('\t')[4])
             bnt = int(line.rstrip().split('\t')[3]) # bowtie nucleotide
             if direction: # positive strand read
-                insertionNt = bnt + readLength - 1 # -2 if I made a +1 error
+                insertionNt = bnt + readLength - 1
                 orient = 'L'
             else: # negative strand read
-                insertionNt = bnt + 1 # delete +1 if I made a +1 error
+                insertionNt = bnt + 1
                 orient = 'R'
             newInsertion = (experiment, sample, contig, insertionNt, orient)
-            insertions.append(newInsertion)
-    return insertions
+            #insertions.append(newInsertion)
+            with open('{0}/temp/{0}_bowtieOutput_InsertionDetail.txt'.format(experiment), 'a') as fo:
+                fo.write('\t'.join(str(item) for item in newInsertion) + '\n')
+    #return insertions
 
-def insertionCounts(bowtieOutput, experiment=''):
+def insertionNucleotidesCount(experiment=''):
     """ List counts for each transposon insertion:
+
+    NOT PRECISE ANYMORE -- UPDATE THIS DOCSTRING
 
     Returns a list of tuples in which the frequence of each orientation in the
     dataset is listed:
@@ -127,44 +140,72 @@ def insertionCounts(bowtieOutput, experiment=''):
     [('Exp002', 'AAAA', 'contig1', 514141, 20, 14, 34),
     ('Exp003', 'GCTA', 'contig5', 8141, 0, 1, 1)]
 
-    Print the output to a file called <experiment>_bowtieOutputSummarized.txt
+    Print output tab-delimited in a file called <experiment>_bowtieOutput_InsertionDetailCount.txt
 
     """
-    # list - each insertion read listed individually
-    li = insertionNucleotides(bowtieOutput)
 
-    # list of hits in bowtie results independent of orientation
-    # add placeholder integers for L, R, and Total counts
-    uniqueHits = list(set([tuple(x for x in y[0:4]) for y in li]))
+    #with open('{0}/temp/{0}_bowtieOutput_InsertionDetail.txt'.format(experiment)) as fi:
+    #    fi_tsv = csv.reader(f, delimiter='\t')
+    #    headers = next(fi_tsv)
+    #    for row in fi_tsv:
+    #        print(row)
+    #
+    #Currently giving IOError: [Errno 2] No such file or directory
 
-    # dictionary of counts of L and R counts
-    # key is the index in the uniqueHits list of tuples
-    counts = {}
-    for i in range(0,len(uniqueHits)):
-        counts[i] = [0, 0, 0]
 
-    # loop through the bowtie output of the insertions
-    # for each insertion...
-    # check the index of the read in the uniqueHits list of tuples
-    # and incremement the L (or R) and Total counts
-    for i in li:
-        hitsIndex = uniqueHits.index(i[0:4])
-        if i[4] == 'L':
-            counts[hitsIndex][0] += 1  # L
-            counts[hitsIndex][2] += 1  # Total
-        if i[4] == 'R':
-            counts[hitsIndex][1] += 1  # R
-            counts[hitsIndex][2] += 1  # Total
+    # Count the insertions (unique per experiment/sample/contig/nucleotide/orientation)
 
-    lo = []
-    for i in range(0,len(uniqueHits)):
-        lo.append(uniqueHits[i] + tuple(counts[i]))
+    """counter = collections.Counter()
 
-    with open('{0}_bowtieOutputSummarized.fastq'.format(experiment), 'w') as fo:
-        # Sort list by first 4 columns
-        for i in sorted(lo, key=itemgetter(0,1,2,3)):
-            fo.write('\t'.join(str(item) for item in i) + '\n')
-    #return(lo)
+    # read the raw insertion data.
+    # Count identical experiment/sample/contig/nucleotide/orientation
+    with open('{0}/temp/{0}_bowtieOutput_InsertionDetail.txt'.format(experiment), 'r') as fi:
+        for line in fi:
+            # create a tuple of the unique hit data
+            tupi = tuple(line.rstrip().split('\t'))
+            counter.update([tupi])
+
+    # Write to tab-delimited file
+    # experiment/sample/contig/nucleotide/orientation/count
+    with open('{0}/temp/{0}_bowtieOutput_InsertionDetailCount__Temp.txt'.format(experiment), 'w') as fo:
+        for tup in counter:
+            for x in tup:
+                fo.write('{0}\t'.format(x.strip()))
+            fo.write('{0}\n'.format(counter[tup]))"""
+
+    # read the raw count data: experiment/sample/contig/nucleotide/orientation/counts
+    with open('{0}/temp/{0}_bowtieOutput_InsertionDetailCount__Temp.txt'.format(experiment), 'r') as fi2:
+        # dictionary of counted tuples
+        # {('Exp002', 'AAAA', 'contig1', 514141): (20, 14, 34)}
+        #       Key = experiment/sample/contig/nucleotide
+        #       Value = Lcounts/Rcounts/totalCounts
+        do = {}
+        for line in fi2:
+            Lcounts = Rcounts = 0
+            tupi = tuple(line.rstrip().split('\t'))
+            tnOrientation = tupi[4]
+            if tnOrientation == 'L':
+                Lcounts = int(tupi[5])
+            if tnOrientation == 'R':
+                Rcounts = int(tupi[5])
+            # e is a single entry of experiment/sample/contig/nucleotide
+            e = tupi[0:4]
+            if e in do:
+                do[e] = (do[e][0]+Lcounts, do[e][1]+Rcounts, do[e][2]+Lcounts+Rcounts)
+            else:
+                do[e] = (Lcounts, Rcounts, Lcounts+Rcounts)
+
+    # Write to tab-delimited file
+    # experiment/sample/contig/nucleotide/Lcount/Rcount/totalCount
+    with open('{0}/temp/{0}_bowtieOutput_InsertionDetailCount.txt'.format(experiment), 'w') as fo2:
+        for e in do:
+            for x in e:
+                fo2.write('{0}\t'.format(x.strip()))
+            for y in do[e]:
+                fo2.write('{0}\t'.format(str(y)))
+            fo2.write('\n')
+
+
 
 def filterCounts(bowtieOutput):
     """ Filter for min 5 reads total (1 each L/R) and maximum 10-fold L/R differential
