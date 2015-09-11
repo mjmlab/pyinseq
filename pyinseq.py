8ec50bb8d3f7754abd177371a8a444e7d4ae2920
Mark Mandel <mandel01@gmail.com>
2015-09-11 01:30:26 -0500

    Entire pipeline works Python2 and uncompressed reads file.

--- pyinseq.py
+++ pyinseq.py
@@ -25,14 +25,16 @@ def parseArgs(args):
         required=True)
     return parser.parseArgs(args)
 
-# Change to the genome directory and run bowtieBuild() and bowtieMap()
-class cdCallBowtie:
-    def __init__(self, tempDir, organism, reads, bowtieOutput):
+# Change to the specified directory
+# http://stackoverflow.com/a/13197763
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+    def __enter__(self):
         self.savedPath = os.getcwd()
-        os.chdir(tempDir)
-        bowtieBuild(organism)
-        bowtieMap(organism, reads, bowtieOutput)
-    def __del__(self):
+        os.chdir(self.newPath)
+    def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
 
 # ===== Start here ===== #
@@ -42,40 +44,54 @@ def main():
     #print(args.input)
     #print(args.samples)
     gbkfile = sys.argv[1]
-    organism = sys.argv[2]
-    experiment = sys.argv[3]
-    reads = sys.argv[4]
-    samples = sys.argv[5]
+    experiment = sys.argv[2]
+    reads = sys.argv[3]
+    samples = sys.argv[4]
+
+    # originally this was to name the organism, e.g. VF for Vfischeri
+    # it really only was used to name the genome files so it doesn't
+    # seem worth to use it as a variable. Will replace it simply
+    # with the word 'genome'.
+    # For now, files generated will be called 'genome.fna' etc
+    # In the future this can be streamlined.
+    organism = 'genome'
 
     #pyinseqDirectory = os.getcwd()
     tempDir = '{0}/temp/'.format(experiment)
 
     # Create the directory struture based on the experiment name
-#    createDirectories(experiment)
+    createDirectories(experiment)
 
     # Prepare genome files from the GenBank input
-#    gbk2fna(gbkfile, organism, tempDir)
-#    gbk2ftt(gbkfile, organism, tempDir)
+    gbk2fna(gbkfile, organism, tempDir)
+    gbk2ftt(gbkfile, organism, tempDir)
 
     # Assign and trim barcodes
     # Now currently filtering by default (16-17 bp, TA at end)
     # In future could add as command line option
-#    assignAndTrim(reads, samples, experiment, tempDir)
+    assignAndTrim(reads, samples, experiment, tempDir)
 
     # Prepare the bowtie indexes
     # Map the reads using bowtie_map
     readsAssigned = '{0}_assigned.fastq'.format(experiment) # already exsists
     bowtieOutput = '{0}_bowtieOutput.txt'.format(experiment) # will get created at next step
-#    x = cdCallBowtie(tempDir, organism, readsAssigned, bowtieOutput)
+
+    # Change directory, build bowtie indexes, call bowtie, change directory back
+    with cd(tempDir):
+        bowtieBuild(organism)
+        bowtieMap(organism, readsAssigned, bowtieOutput)
+
+
+    #x = cdCallBowtie(tempDir, organism, readsAssigned, bowtieOutput)
 
     # Summarize the bowtie results
     # Need more consistency in how directory locations are handled
-#    insertionNucleotides(tempDir + bowtieOutput, experiment)
-#    insertionNucleotidesCount(experiment)
-#    filterSortCounts(experiment)
+    insertionNucleotides(tempDir + bowtieOutput, experiment)
+    insertionNucleotidesCount(experiment)
+    filterSortCounts(experiment)
     #insertionCounts(experiment) ## FOR TESTING ONLY - DOES NOT WRITE DATA
-#    normalizeCpm(experiment) ## FOR TESTING ONLY UNLESS WE WANT IT TO WRITE DATA
-#    mapToGene(organism, experiment) ## FOR TESTING ONLY UNLESS WE WANT IT TO WRITE DATA
+    #normalizeCpm(experiment) ## FOR TESTING ONLY UNLESS WE WANT IT TO WRITE DATA
+    #mapToGene(organism, experiment) ## FOR TESTING ONLY UNLESS WE WANT IT TO WRITE DATA
     mapToGeneSummary(1.0, organism, experiment)  # First parameter is threePrimeness cutoff
 
 if __name__ == '__main__':
