969ac61aa35860934e693a56cf486ef57d983406
Mark Mandel <mandel01@gmail.com>
2015-09-21 23:35:37 -0500

    simplified config file

--- config.py
+++ config.py
@@ -1,26 +1,13 @@
 """ Configuration file for pyinseq.py package """
 
-# Experiment name
-# Used as a prefix for naming files and samples
-# Alphanumeric characters only, no spaces
-experiment = ''
-
-# Genome file in GenBank format (concatenated GenBank if multiple contigs)
-gb = ''
-
-# Barcodes (5' barcodes)
-sampleFile = ''
-
-# Transposon end sequences
-# In many cases the transposon sequence will be the same for both ends.
-# This sequence is filtered from the reads prior to mapping.
-# For mariner transposons do not include the terminal TA. Mariner hops into
-# TA dinucleotides and adding the TA here will cause those nucleotides to
-# be filtered prior to quality checking and mapping. Defaults are sequences for
-# typical mariner ends.
+# TRANSPOSON END SEQUENCES
+# Typical mariner transposon ends for INSeq are the defaults (same left/right ends)
+# This sequence is filtered from the reads prior to mapping
+# Mariner transposons inserts at TA dinucleotides; do not include the corresponding
+# TA from the tranpsoson here
 transposonLeft = 'ACAGGTTG'
 transposonRight = 'ACAGGTTG'
 
-# Path to Bowtie
+# PATH TO BOWTIE
 bowtie = '/Users/markmandel/bowtie-1.1.1/bowtie'
 bowtieBuild = '/Users/markmandel/bowtie-1.1.1/bowtie-build'
