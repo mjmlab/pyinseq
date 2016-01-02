855869eeaf8918a0c2276b1c4e9d52176be2e7fd
Mark Mandel <mandel01@gmail.com>
2016-01-02 00:37:58 -0600

    add --nobarcodes option to use samples that have barcodes already removed. Will document once it is integrated better into the pipeline.

--- pyinseq.py
+++ pyinseq.py
@@ -1,77 +1,93 @@
 #!/usr/bin/env python
-"""
-Main script for running the pyinseq package
+"""Main script for running the pyinseq package."""
 
-"""
-
-#from assign import *
-from demultiplex import *
-from gbkconvert import *
-from mapReads import *
-from processMapping import *
-from utils import *
-import sys
-import os
 import argparse
+import os
+from shutil import copyfile
+import sys
+from demultiplex import sample_prep, demultiplex_fastq, trim_fastq
+from gbkconvert import gbk2fna, gbk2ftt
+from mapReads import bowtieBuild, bowtieMap
+from processMapping import mapSites, mapGenes, buildGeneTable
+from utils import convert_to_filename, createExperimentDirectories
+
 
 def parseArgs(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input',
-        help='input Illumina reads file',
-        required=True)
-    parser.add_argument('-s', '--samples',
-        help='sample list with barcodes',
-        required=True)
-    parser.add_argument('-e', '--experiment',
-        help='experiment name (no spaces or special characters)',
-        required=True)
-    parser.add_argument('-g', '--genome',
-        help='genome in GenBank format (one concatenated file for multiple contigs/chromosomes)',
-        required=True)
-    parser.add_argument('-d', '--disruption',
-        help='fraction of gene disrupted (0.0 - 1.0)',
-        default=1.0)
+    parser.add_argument('-i',
+                        '--input',
+                        help='input Illumina reads file',
+                        required=True)
+    parser.add_argument('-s',
+                        '--samples',
+                        help='sample list with barcodes',
+                        required=True)
+    parser.add_argument('-e',
+                        '--experiment',
+                        help='experiment name (no spaces or special characters)',
+                        required=True)
+    parser.add_argument('-g',
+                        '--genome',
+                        help='genome in GenBank format (one concatenated file for multiple contigs/chromosomes)',
+                        required=True)
+    parser.add_argument('-d',
+                        '--disruption',
+                        help='fraction of gene disrupted (0.0 - 1.0)',
+                        default=1.0)
+    parser.add_argument('--nobarcodes',
+                        help='barcodes have already been removed from the samples; \
+                        -i should list the directory with filenames (.fastq.gz) \
+                        corresponding to the sample names',
+                        action='store_true',
+                        default=False)
     return parser.parse_args(args)
 
-# Change to the specified directory
+
 class cd:
-    """Context manager for changing the current working directory"""
+
+    """Change to the current working directory, then back."""
+
     def __init__(self, newPath):
         self.newPath = os.path.expanduser(newPath)
+
     def __enter__(self):
         self.savedPath = os.getcwd()
         os.chdir(self.newPath)
+
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
 
-# ===== Start here ===== #
 
 def main():
+    """Start here."""
     args = parseArgs(sys.argv[1:])
     gbkfile = args.genome
     experiment = convert_to_filename(args.experiment)
     reads = args.input
     samples = args.samples
+    # TODO(Test that disruption is between 0.0 and 1.0 (or absent, default 1.0))
     disruption = float(args.disruption)
-    #TODO: Test that disruption is between 0.0 and 1.0 (or absent, default 1.0)
+    nobarcodes = args.nobarcodes
 
-    # originally this was to name the organism, e.g. VF for Vfischeri
-    # it really only was used to name the genome files so it doesn't
-    # seem worth to use it as a variable. Will replace it simply
-    # with the word 'genome'.
-    # For now, files generated will be called 'genome.fna' etc
-    # In the future this can be streamlined.
+    # Lookup files generated will be called 'genome.fna' etc
     organism = 'genome'
 
     # pyinseqDirectory = os.getcwd()
     genomeDir = '{experiment}/genome_lookup/'.format(experiment=experiment)
-    # Create the directory struture based on the experiment name
-    createExperimentDirectories(experiment)
+
+    # Note: barcode length hardcoded at 4 bp here
+    barcode_qc, barcode_length = True, 4
+    if nobarcodes:
+        barcode_qc, barcode_length = False, 0
 
     # samples dictionary
     # samples = OrderedDict([('name1', {'name': 'name1', 'barcode': 'barcode1'}),
     #    ('name2', {'name': 'name2', 'barcode': 'barcode2'})])
-    samplesDict = sample_prep(samples)
+    samplesDict = sample_prep(samples, barcode_qc)
+
+    # Create the directory struture based on the experiment name
+    createExperimentDirectories(experiment)
+
     # add 'demultiplexedPath' and 'trimmedPath' fields for each sample
     for sample in samplesDict:
         demultiplexedPath = '{experiment}/raw_data/{sampleName}.fastq.gz'.format(
@@ -83,8 +99,18 @@ def main():
         samplesDict[sample]['demultiplexedPath'] = demultiplexedPath
         samplesDict[sample]['trimmedPath'] = trimmedPath
 
-    #demultiplex based on barcodes defined in the sample file
-    demultiplex_fastq(reads, samplesDict, experiment)
+    if nobarcodes:
+        # copy reads files into the experiment/raw_data directory
+        for sample in samplesDict:
+            # makes sure the reads directory has a trailing slash
+            if reads[-1] != '/':
+                reads += '/'
+            src = reads + sample + '.fastq.gz'
+            dst = samplesDict[sample]['demultiplexedPath']
+            copyfile(src, dst)
+    else:
+        # demultiplex based on barcodes defined in the sample file
+        demultiplex_fastq(reads, samplesDict, experiment)
 
     # Prepare genome files from the GenBank input
     gbk2fna(gbkfile, organism, genomeDir)
@@ -98,7 +124,7 @@ def main():
     geneMappings = {}
     for sample in samplesDict:
         s = samplesDict[sample]
-        trim_fastq(s['demultiplexedPath'], s['trimmedPath'], sample)
+        trim_fastq(s['demultiplexedPath'], s['trimmedPath'], sample, barcode_length)
         # Change directory, map to bowtie, change directory back
         trimmedSampleFile = '{0}_trimmed.fastq'.format(sample)
         bowtieOutputFile = '{0}_bowtie.txt'.format(sample)
