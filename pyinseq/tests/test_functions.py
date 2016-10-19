d74d0533f4e457cdfd28eedd99bdf75680679aba
Camille Scott <camille.scott.w@gmail.com>
2016-10-19 15:59:15 -0700

    add expected output, fixtures for test data, test of script

--- pyinseq/tests/test_functions.py
+++ pyinseq/tests/test_functions.py
@@ -2,30 +2,31 @@
 import csv
 from collections import OrderedDict
 #from io import StringIO
-import pyinseq
+from pyinseq.runner import Settings, tab_delimited_samples_to_dict
+from pyinseq import utils
 import numpy as np
 import pandas as pd
 
 # pyinseq.utils
 
 def test_filename_replace_spaces():
-    assert pyinseq.utils.convert_to_filename('file name') == 'file_name'
+    assert utils.convert_to_filename('file name') == 'file_name'
 
 def test_filename_leading_whitespace():
-    assert pyinseq.utils.convert_to_filename('  filename') == 'filename'
+    assert utils.convert_to_filename('  filename') == 'filename'
 
 def test_filename_trailing_whitespace():
-    assert pyinseq.utils.convert_to_filename('filename  ') == 'filename'
+    assert utils.convert_to_filename('filename  ') == 'filename'
 
 def test_filename_funny_characters():
-    assert pyinseq.utils.convert_to_filename('filename*&*&*$$') == 'filename'
-    assert pyinseq.utils.convert_to_filename('file*&*&*$$name') == 'filename'
+    assert utils.convert_to_filename('filename*&*&*$$') == 'filename'
+    assert utils.convert_to_filename('file*&*&*$$name') == 'filename'
 
 
 # pyinseq.pyinseq
 
 def test_class_Settings_init():
-    x = pyinseq.Settings('example')
+    x = Settings('example')
     assert x.path == 'results/example/'
     assert x.genome_path == 'results/example/genome_lookup/'
     assert x.raw_path == 'results/example/raw_data/'
@@ -37,17 +38,17 @@ def test_class_Settings_init():
 def test_tab_delimited_samples_to_dict_trailing_newline():
     #s = StringIO('sample_1\tAAAA\tsample_2\tTTTT\n')
     s = 'pyinseq/tests/testdata/sample01_01.txt'
-    assert pyinseq.tab_delimited_samples_to_dict(s) == \
+    assert tab_delimited_samples_to_dict(s) == \
         OrderedDict([('sample_1', {'barcode': 'AAAA'}), ('sample_2', {'barcode': 'TTTT'})])
 
 def test_tab_delimited_samples_to_dict_no_trailing_newline():
     #s = StringIO('sample_1\tAAAA\tsample_2\tTTTT\n')
     s = 'pyinseq/tests/testdata/sample01_02.txt'
-    assert pyinseq.tab_delimited_samples_to_dict(s) == \
+    assert tab_delimited_samples_to_dict(s) == \
         OrderedDict([('sample_1', {'barcode': 'AAAA'}), ('sample_2', {'barcode': 'TTTT'})])
 
 def test_tab_delimited_samples_to_dict_no_trailing_newline():
     #s = StringIO('sample_1\tAAAA\tsample_2\tTTTT\n')
     s = 'pyinseq/tests/testdata/sample01_02.txt'
-    assert pyinseq.tab_delimited_samples_to_dict(s) == \
+    assert tab_delimited_samples_to_dict(s) == \
         OrderedDict([('sample_1', {'barcode': 'AAAA'}), ('sample_2', {'barcode': 'TTTT'})])
