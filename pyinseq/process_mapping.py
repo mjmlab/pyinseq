2cdf6b83f0cc1da0ae845145bb0aa38e546c61b0
Mark Mandel <mmandel@wisc.edu>
2019-02-16 09:36:34 -0600

    Fix three_primeness math

--- pyinseq/process_mapping.py
+++ pyinseq/process_mapping.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Counts the bowtie hits at each position in each sample
+Count and process the bowtie hits at each position in each sample
 
 """
 
@@ -14,13 +14,12 @@ def map_sites(sample, samples_dict, settings):
     map_dict = {}
     # overall_total = denominator for cpm calculation
     overall_total = 0
-    cpm = 0
     bowtie_file = settings.path + sample + "_bowtie.txt"
     sites_file = settings.path + sample + "_sites.txt"
     with open(bowtie_file, "r") as fi:
         for line in fi:
             bowtie_data = line.rstrip().split("\t")
-            # Calculate transposon insertion point = transposonNT
+            # Calculate transposon insertion point = insertion_NT
             contig, insertion_NT, read_length = (
                 str(bowtie_data[2]),
                 int(bowtie_data[3]),
@@ -28,13 +27,13 @@ def map_sites(sample, samples_dict, settings):
             )
             if bowtie_data[1] == "+":  # positive strand read
                 insertion_NT = insertion_NT + read_length - 1
-                map_dict.setdefault((contig, insertion_NT), [0, 0])[0] += 1  # Lcount
+                map_dict.setdefault((contig, insertion_NT), [0, 0])[0] += 1  # left
             else:  # negative strand read
                 insertion_NT = insertion_NT + 1
-                map_dict.setdefault((contig, insertion_NT), [0, 0])[1] += 1  # Rcount
+                map_dict.setdefault((contig, insertion_NT), [0, 0])[1] += 1  # right
             overall_total += 1
     # write tab-delimited of contig/nucleotide/left_counts/right_counts/total_counts/cpm
-    # use the index totalCounts as the denominator for cpm calculation
+    # use the total_counts as the denominator for cpm calculation
     with open(sites_file, "a") as fo:
         writer = csv.writer(fo, delimiter="\t", dialect="excel")
         header_entry = (
@@ -90,9 +89,6 @@ def map_genes(sample, settings):
     gene_dict = {}
     sites_file = settings.path + sample + "_sites.txt"
     genes_file = settings.path + sample + "_genes.txt"
-    # TODO(minimum counts and maximum ratio)\
-    # min_counts = settings.min_counts
-    # max_ratio = settings.max_ratio
     with open(sites_file, "r", newline="") as csvfileR:
         sites_reader = csv.reader(csvfileR, delimiter="\t")
         next(sites_reader, None)  # skip the headers
@@ -107,41 +103,40 @@ def map_genes(sample, settings):
                     gene[0],
                     int(gene[1]),
                     int(gene[2]),
-                    *gene[3:11],
+                    gene[3],
+                    int(gene[4]),
+                    *gene[5:11],
                 )
                 # contig from insertion; locus from lookup table
                 if contig == locus:
-                    # TODO: simplify nucleotide location match with gene
-                    if nucleotide >= start:
-                        if nucleotide <= end:
-                            # 0.0 = 5'end ; 1.0 = 3'end
-                            # TODO: Should featureEnd have +1 added?
-                            if strand == "+":
-                                three_primeness = (nucleotide - start) / (end - start)
-                            if strand == "-":
-                                three_primeness = (end - nucleotide) / (end - start)
-                            mappedHit = (
-                                contig,
-                                nucleotide,
-                                left_counts,
-                                right_counts,
-                                total_counts,
-                                cpm,
-                                three_primeness,
-                                locus_tag,
-                            )
-                            # Checks minimum count and max ratio
-                            if int(total_counts) >= settings.min_counts and (
-                                min(int(left_counts), int(right_counts))
-                                * settings.max_ratio
-                            ) >= max(int(left_counts), int(right_counts)):
-                                mapped_hit_list.append(mappedHit)
-                                # Filter based on location in the gene
-                                if three_primeness <= settings.disruption:
-                                    # Add to the total for that gene --
-                                    # Single-element list (rather than integer) so
-                                    # that it is subscriptable to add cpm counts
-                                    gene_dict.setdefault(locus_tag, [0])[0] += cpm
+                    if start <= nucleotide <= end:
+                        # 0.0 = 5'end ; 1.0 = 3'end
+                        if strand == "+":
+                            three_primeness = (1 + nucleotide - start) / length
+                        else:
+                            three_primeness = (end - nucleotide) / length
+                        mappedHit = (
+                            contig,
+                            nucleotide,
+                            left_counts,
+                            right_counts,
+                            total_counts,
+                            cpm,
+                            three_primeness,
+                            locus_tag,
+                        )
+                        # Checks minimum count and max ratio
+                        if int(total_counts) >= settings.min_counts and (
+                            min(int(left_counts), int(right_counts))
+                            * settings.max_ratio
+                        ) >= max(int(left_counts), int(right_counts)):
+                            mapped_hit_list.append(mappedHit)
+                            # Filter based on location in the gene
+                            if three_primeness <= settings.disruption:
+                                # Add to the total for that gene --
+                                # Single-element list (rather than integer) so
+                                # that it is subscriptable to add cpm counts
+                                gene_dict.setdefault(locus_tag, [0])[0] += cpm
                 previous_feature = locus_tag
     # Write individual insertions to *_genes.txt
     with open(genes_file, "w", newline="") as csvfileW:
@@ -169,7 +164,6 @@ def build_gene_table(organism, sample_dict, gene_mappings, experiment=""):
        for each sample in the experiment
     """
 
-    # TODO(Bring back in the header row in the future. Use it here; ignore it for previous steps)
     gene_table = ftt_lookup(organism, experiment)
 
     # Header will be extended in the future to
@@ -206,15 +200,12 @@ def build_gene_table(organism, sample_dict, gene_mappings, experiment=""):
             row.append(0)
         # add the sample's results to the building gene_table
         mapped_genes = gene_mappings[sample]
-        # mapped_genes = gene_mappings.get(sampleName)
-        # TODO(simplify this:)
         # for each row in the table, *try* from mapped_genes. Add if found.
         for gene in mapped_genes:
             for i, f in enumerate(gene_table):
                 hit_locus_tag = gene
                 ftt_locus_tag = f[7]
-                # matches based on locusTag.
-                # In future should I instead create an index field in the .ftt?
+                # matches based on locus_tag.
                 if hit_locus_tag == ftt_locus_tag:
                     gene_table[i][current_column] += mapped_genes[gene][0]
         with open(f"results/{experiment}/summary_gene_table.txt", "w") as fo:
@@ -228,9 +219,6 @@ def ftt_lookup(organism, experiment=""):
        {'VF_0001': {'locus': 'CP000020', 'start': ...},
            'VF_0002': {'locus': 'CP000020', 'start': ...}}
     """
-
-    # TODO: Error checking when generating the ftt file that locus tags are \
-    # unique and complete.
     ftt_list = []
     with open(
         f"results/{experiment}/genome_lookup/{organism}.ftt", newline=""
